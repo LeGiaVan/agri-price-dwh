fdb886dc16cace27329516792e93151b3d8c6949
Quynh Thu <quynhthu0921@gmail.com>
2026-05-31 15:34:06 +0700

    feat: hoàn thành nạp dữ liệu 5 mặt hàng (Coffee, Rice, Palm Oil, Cocoa, Cotton/Rubber) từ YF và WB

--- db_init.py
+++ db_init.py
@@ -2,7 +2,7 @@ import duckdb, os
 from dotenv import load_dotenv
 load_dotenv()
 
-con = duckdb.connect(f"md:?motherduck_token={os.getenv('MOTHERDUCK_TOKEN')}")
+con = duckdb.connect(f"md:agri_dwh?motherduck_token={os.getenv('MOTHERDUCK_TOKEN')}")
 con.execute("CREATE DATABASE IF NOT EXISTS agri_dwh")
 con.execute("USE agri_dwh")
 con.execute("CREATE SCHEMA IF NOT EXISTS bronze")
